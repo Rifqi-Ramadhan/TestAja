a66969b0f3e80ce73c686b3f4dfd167b116da246
Rifqi-Ramadhan <rifqi.ramadhan22@ui.ac.id>
2023-09-07 22:18:01 +0700

    TP01-Kalkulator Sederhana

---
+++ Aritmatis.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class Aritmatis{
+	
+	public static void main(String[] args){
+	Scanner sc = new Scanner(System.in);
+	System.out.println ("Masukan Angka Pertama :");
+	float x = sc.nextFloat();
+	System.out.println ("Masukan Angka Kedua :");
+	float y = sc.nextFloat();
+	System.out.println("Hasil Penjumlahan adalah :");
+	System.out.println(x+y);
+	System.out.println("Hasil Pengurangan adalah :");
+	System.out.println(x-y);
+	System.out.println("Hasil Perkalian adalah :");
+	System.out.println(x*y);
+	System.out.println("Hasil pembagian adalah :");
+	System.out.println(x/y);
+	
+	}
+	
+}
